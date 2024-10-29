00fbb97d32e70fabd732bdac2180b64180f5483c
Chloe <clolanglois165@gmail.com>
2024-10-29 12:17:22 -0700

    Final footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,39 +5,54 @@
     <section id="footer-content">
         <div id="div-footer1">
             <h2>Ecosalub Laval</h2>
-            <p>3060 rue Peugeot</p>
-            <p>Laval, QC H7L5C5</p>
-            <p>CA</p>
+            <a
+                href="https://www.google.com/maps/place/3060+Rue+Peugeot,+Laval,+QC+H7L+5C5/@45.5876419,-73.7470242,16z/data=!3m1!4b1!4m6!3m5!1s0x4cc9215c5506270b:0x7d7c9922f3e84d7d!8m2!3d45.5876419!4d-73.7470242!16s%2Fg%2F11f64gsp28?entry=ttu&g_ep=EgoyMDI0MTAyMy4wIKXMDSoASAFQAw%3D%3D">
+                <p>3060 rue Peugeot</p>
+                <p>Laval, QC H7L5C5, CA</p>
+            </a>
             <br>
-            <p>(450) 241-1234</p>
-            <p>info@ecosalub.ca</p>
+            <p><a href="tel:+14502411234">(450) 241-1234</a></p>
+            <p><a href="tel:+18334601581">(833) 460-1581</a></p>
+            <p><a href="mailto:info@ecosalub.ca">info@ecosalub.ca</a></p>
+
 
             <div id="icons">
                 <a href="https://www.facebook.com/profile.php?id=100093387998944"><img
                         src="{{ asset('img/facebook.png') }}" alt="Visit our Facebook page"></a>
                 <a href="tel:+14502411234"><img src="{{ asset('img/telephone.png') }}" alt="Call us"></a>
                 <a href="mailto:info@ecosalub.ca"><img src="{{ asset('img/envelope.png') }}" alt="Send Email"></a>
-                <a href="https://www.linkedin.com/company/ecosalub-laval-toronto/?viewAsMember=true"><img src="{{ asset('img/linkedin.png') }}" alt="LinkedIn"></a>
+                <a href="https://www.linkedin.com/company/ecosalub-laval-toronto/?viewAsMember=true"><img
+                        src="{{ asset('img/linkedin.png') }}" alt="LinkedIn"></a>
             </div>
-            <iframe class="map" <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5584.288794383871!2d-73.7470242!3d45.5876419!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc9215c5506270b%3A0x7d7c9922f3e84d7d!2s3060%20Rue%20Peugeot%2C%20Laval%2C%20QC%20H7L%205C5!5e0!3m2!1sen!2sca!4v1730227260969!5m2!1sen!2sca" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe class="map"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5584.288794383871!2d-73.7470242!3d45.5876419!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc9215c5506270b%3A0x7d7c9922f3e84d7d!2s3060%20Rue%20Peugeot%2C%20Laval%2C%20QC%20H7L%205C5!5e0!3m2!1sen!2sca!4v1730227260969!5m2!1sen!2sca"
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <div id="div-footer2">
             <h2>Ecosalub Toronto</h2>
-            <p>245, Unit 15, Boul. Matheson E.</p>
-            <p>Mississauga, Ontario, L4Z 3K2</p>
-            <p>CA</p>
+            <a href="https://www.google.com/maps/place/2601+Matheson+Blvd+E,+Mississauga,+ON+L4W+5A8/@43.6592404,-79.600637,16z/data=!4m6!3m5!1s0x882b3854b033328f:0x5110d53a3049dfa4!8m2!3d43.6592404!4d-79.600637!16s%2Fg%2F11c3q3cmb_?entry=ttu&g_ep=EgoyMDI0MTAyMy4wIKXMDSoASAFQAw%3D%3D">
+                <p>245, Unit 15, Boul. Matheson E.</p>
+                <p>Mississauga, Ontario, L4Z 3K2, CA</p>
+            </a>
             <br>
-            <p>(450) 241-1234</p>
-            <p>info@ecosalub.ca</p>
+            <p><a href="tel:+14502411234">(450) 241-1234</a></p>
+            <p><a href="tel:+18334601581">(833) 460-1581</a></p>
+            <p><a href="mailto:info@ecosalub.ca">info@ecosalub.ca</a></p>
+
 
             <div id="icons">
                 <a href="https://www.facebook.com/profile.php?id=100093387998944"><img
-                    src="{{ asset('img/facebook.png') }}" alt="Visit our Facebook page"></a>
-            <a href="tel:+14502411234"><img src="{{ asset('img/telephone.png') }}" alt="Call us"></a>
-            <a href="mailto:info@ecosalub.ca"><img src="{{ asset('img/envelope.png') }}" alt="Send Email"></a>
-            <a href="https://www.linkedin.com/company/ecosalub-laval-toronto/?viewAsMember=true"><img src="{{ asset('img/linkedin.png') }}" alt="LinkedIn"></a>
+                        src="{{ asset('img/facebook.png') }}" alt="Visit our Facebook page"></a>
+                <a href="tel:+18334601581"><img src="{{ asset('img/telephone.png') }}" alt="Call us"></a>
+                <a href="mailto:info@ecosalub.ca"><img src="{{ asset('img/envelope.png') }}" alt="Send Email"></a>
+                <a href="https://www.linkedin.com/company/ecosalub-laval-toronto/?viewAsMember=true"><img
+                        src="{{ asset('img/linkedin.png') }}" alt="LinkedIn"></a>
             </div>
-            <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5772.935549499578!2d-79.60063699999998!3d43.6592404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b3854b033328f%3A0x5110d53a3049dfa4!2s2601%20Matheson%20Blvd%20E%2C%20Mississauga%2C%20ON%20L4W%205A8!5e0!3m2!1sen!2sca!4v1730227340001!5m2!1sen!2sca" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe class="map"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5772.935549499578!2d-79.60063699999998!3d43.6592404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b3854b033328f%3A0x5110d53a3049dfa4!2s2601%20Matheson%20Blvd%20E%2C%20Mississauga%2C%20ON%20L4W%205A8!5e0!3m2!1sen!2sca!4v1730227340001!5m2!1sen!2sca"
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </section>
 </footer>
